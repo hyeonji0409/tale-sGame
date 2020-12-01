551bad6489066ee9c5b9c05e1666b877b5ddc2f2
hyeonji0409 <guswl8507@naver.com>
2020-12-02 01:47:21 +0900

    warning, Quiz_1 추가

---
+++ Assets/Scripts/Quiz_1.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Quiz_1 : MonoBehaviour
+{
+
+    public Text ResultText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ResultText.text = "";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ClickButton1()
+    {
+        ResultText.text = "TRY AGAIN";
+    }
+
+    public void ClickButton2()
+    {
+        SceneManager.LoadScene("realcloudhouse");
+    }
+
+    public void ClickButton3()
+    {
+        ResultText.text = "TRY AGAIN";
+    }
+
+    public void ClickButton4()
+    {
+        ResultText.text = "TRY AGAIN";
+    }
+
+}
